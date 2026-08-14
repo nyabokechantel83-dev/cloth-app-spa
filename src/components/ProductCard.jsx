20cd07670833df44f790d2b7a932f953dfbd53c3
Josephine Njoki Njuguna <josephine.njuguna@student.moringaschool.com>
2026-08-14 22:26:01 +0300

    created a reusable ProduvtCard component that displays product image, title and price and a link to theProductDetails

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,33 @@
+import  {Link} from "react-router-dom"
+
+function ProductCard({ product}) {
+    return (
+        <div className="rounded-xl bg-white p-4 ">
+            <img className="h-64 w-full rounded-lg object-cover"
+              src={product.img}
+              alt={product.name}
+            />
+
+            <div className="mt-4">
+                <h3 className="text-lg font-semibold">
+                    {product.name}
+                </h3>
+
+                <p className="mt-2 text-gray-600">
+                    ksh {product.price}
+                </p>
+
+                <Link
+                   to={`/products/${product.id}`}
+                   className="mt-4 inline-block rounded-lg bg-orange-500 px-4 py-2 font-semibold text-white"
+                >
+                    View Details
+                </Link>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default ProductCard;
