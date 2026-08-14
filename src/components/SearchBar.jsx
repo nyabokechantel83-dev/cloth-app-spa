efec7b17eac1f84fde10d567514a9468e57608f2
Josephine Njoki Njuguna <josephine.njuguna@student.moringaschool.com>
2026-08-14 22:34:12 +0300

    created a search component which creates a search input for products

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function SearchBar({ onSearch }) {
+    const [searchTerm, setSearchTerm] = useState("");
+
+
+function handleChange(event) {
+    const value = event.target.value;
+
+    setSearchTerm(value);
+    onSearch(value);
+}
+return (
+    <div className="mb-8">
+      <label className="mb-2 block font-semibold"
+         htmlFor="product-search"
+      >
+        Search Products
+      </label> 
+
+      <input className="w-full rounded-lg border-gray-300 px-4 py-3 outline-none" 
+       id="product-search"
+       type="text"
+       value={searchTerm}
+       onChange={handleChange}
+       placeholder="Search for a product ...."
+      />   
+    </div>
+
+  );
+}
+
+export default SearchBar;
