693fee25a38a3e4e634a3f1dbe61d7cfe500116a
chantel <nyabokechantel83@gmail.com>
2026-08-14 09:42:36 +0300

    Set up Tailwind and Vitest

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,12 @@ export default defineConfig({
 
   test: {
     environment: "jsdom",
-    setupFiles: "./src/tests/setup.js",
     globals: true,
+    setupFiles: "./src/tests/setup.js",
+
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html"],
+    },
   },
 });
