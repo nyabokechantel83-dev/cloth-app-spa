02ef2203da526a9f01876ae16b2ed09cadd81f49
Josephine Njoki Njuguna <josephine.njuguna@student.moringaschool.com>
2026-08-16 01:40:04 +0300

    fixed the SearchBar input and search callback

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,32 +1,33 @@
 import { useState } from "react";
 
 function SearchBar({ onSearch }) {
-    const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
-
-function handleChange(event) {
+  function handleChange(event) {
     const value = event.target.value;
 
     setSearchTerm(value);
     onSearch(value);
-}
-return (
+  }
+
+  return (
     <div className="mb-8">
-      <label className="mb-2 block font-semibold"
-         htmlFor="product-search"
+      <label
+        className="mb-2 block font-semibold"
+        htmlFor="product-search"
       >
         Search Products
-      </label> 
+      </label>
 
-      <input className="w-full rounded-lg border-gray-300 px-4 py-3 outline-none" 
-       id="product-search"
-       type="text"
-       value={searchTerm}
-       onChange={handleChange}
-       placeholder="Search for a product ...."
-      />   
+      <input
+        className="w-full rounded-lg border border-gray-300 px-4 py-3 outline-none"
+        id="product-search"
+        type="text"
+        value={searchTerm}
+        placeholder="Search for a product..."
+        onChange={handleChange}
+      />
     </div>
-
   );
 }
 
