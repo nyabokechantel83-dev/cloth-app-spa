8b2610b64c394c30ddf8f7a1fae4d066749dadd5
chantel <nyabokechantel83@gmail.com>
2026-08-18 09:58:31 +0300

    update Home page design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -83,7 +83,7 @@ function Home() {
       </section>
 
       <footer className="bg-gray-900 px-6 py-5 text-center text-sm text-white">
-        © 2026 Cloth App. All rights reserved.
+        © 2026 JOVETA CLOTHLINE. All rights reserved.
       </footer>
     </main>
   );
