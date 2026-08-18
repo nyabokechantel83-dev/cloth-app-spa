b2e008c38ff6614ecbb1e22bec38bd829b587226
chantel <nyabokechantel83@gmail.com>
2026-08-18 12:11:38 +0300

    Add order success route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,23 +2,29 @@ import { NavLink } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="flex justify-between items-center p-4 bg-rose-100 text-gray-800">
-      <h1 className="text-xl font-bold text-rose-600">JOVETA CLOTHLINE</h1>
+    <nav className="flex justify-between items-center px-6 py-5 bg-orange-100 text-gray-800">
+      <h1 className="text-3xl font-extrabold text-orange-600">
+        JOVETA CLOTHLINE
+      </h1>
 
       <div className="flex gap-6">
-        <NavLink to="/" className="hover:text-rose-600">
+        <NavLink to="/" className="hover:text-orange-600">
           Home
         </NavLink>
 
-        <NavLink to="/shop" className="hover:text-rose-600">
+        <NavLink to="/shop" className="hover:text-orange-600">
           Shop
         </NavLink>
 
-        <NavLink to="/cart" className="hover:text-rose-600">
+        <NavLink to="/cart" className="hover:text-orange-600">
           Cart
         </NavLink>
 
-        <NavLink to="/admin" className="hover:text-rose-600">
+        <NavLink to="/checkout" className="hover:text-orange-600">
+          Checkout
+        </NavLink>
+
+        <NavLink to="/admin" className="hover:text-orange-600">
           Admin
         </NavLink>
       </div>
