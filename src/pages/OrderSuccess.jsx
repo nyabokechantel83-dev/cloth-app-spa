c1759e25b1f6c2efe718c169b7c30ff1a0152e3b
Josephine Njoki Njuguna <josephine.njuguna@student.moringaschool.com>
2026-08-18 12:38:22 +0300

    added the footer

--- src/pages/OrderSuccess.jsx
+++ src/pages/OrderSuccess.jsx
@@ -28,6 +28,11 @@ function OrderSuccess() {
           Continue Shopping
         </Link>
       </div>
+
+      <footer className="bg-gray-900 px-6 py-5 text-center text-sm text-white">
+        © 2026 JOVETA CLOTHLINE. All rights reserved.
+      </footer>
+      
     </div>
   );
 }
